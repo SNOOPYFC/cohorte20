4ef67057d7ca276754ddc7009b415fb11cfbbf9e
unknown <shalnarkf@gmail.com>
2022-12-21 21:27:43 -0600

    f

---
+++ miPagina/assets/javascript/javascript.js
@@ -0,0 +1,18 @@
+//para parrafo
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.documentElement.scrollTop > 200) {
+    document.getElementById("menu").className = "slideUp";
+  } else    {document.getElementById("menu").className = "slideUps";
+    
+  }
+}
+
+//para icono
+
+
+
+
+
+
