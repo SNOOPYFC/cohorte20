1c7c5bbbd87e5e22f68a16cd9b8d7ff9338a96ae
unknown <shalnarkf@gmail.com>
2022-12-21 21:33:18 -0600

    d

---
+++ TAREA/assets/javascript.js
@@ -0,0 +1,31 @@
+// EJERCICIOS DE ALGORITMO
+let num1 = prompt("Dame un numero:")
+let num2 = prompt("Dame un numero:")
+let num3 = prompt("Dame un numero:")
+const array = [num1,num2,num3]
+const sort = array.sort()
+console.log("Ascendentes: ", sort);
+const reversed = array.reverse();
+console.log('Descendentes:', reversed);
+let simon =array[0]
+let claro = array.reverse()
+let coco = claro[0]
+if (simon === coco){
+  console.log("LOS NUMEROS SON IGUALES")
+}
+// let num1 = prompt("Dame un numero:")
+// let num2 = prompt("Dame un numero:")
+// let num3 = prompt("Dame un numero:")
+// const array = [num1,num2,num3] 
+// const yarra = [num1,num2,num3]
+// console.log("Ascendentes: ", array.sort());
+// console.log('Descendentes:', yarra.reverse());
+// let simon = array.sort()
+// let claro = yarra.reverse()
+// let caiman= simon[0]
+// let coco = claro[0]
+// if (caiman === coco){
+//   console.log("LOS NUMEROS SON IGUALES")
+// }
+// console.log(simon);
+// console.log(claro);
